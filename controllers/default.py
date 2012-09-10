6c99e70e04f772db1433c673612bb1f07c3a7360
niphlod@gmail.com <niphlod@gmail.com>
2012-09-10 20:35:47 +0200

    separate config for torrent clients and see torrents stats in a new page

--- controllers/default.py
+++ controllers/default.py
@@ -19,6 +19,8 @@
 
 from gluon.storage import Storage
 from w2p_tvseries_utils import w2p_tvseries_settings
+from w2p_tvseries_clients import w2p_tvseries_torrent_client_loader
+from gluon.contrib import simplejson as sj
 
 def index():
     settings_ = w2p_tvseries_settings()
@@ -93,3 +95,62 @@ def settings():
         response.flash = 'errors in form, please check'
 
     return dict(form=form)
+
+def client_settings():
+    settings_ = w2p_tvseries_settings()
+    settings = settings_.torrent_client_settings()
+    gsettings = settings_.global_settings()
+
+    for k,v in gsettings.iteritems():
+        settings.defaults[k] = v
+
+    fi = [
+        Field(k, settings.types.get(k, 'string'), comment=settings.comments[k],
+              default=settings.defaults[k], label=settings.labels.get(k, k), widget=settings.widgets[k], requires=settings.requires[k])
+        for k in settings.fields
+    ]
+
+    form = SQLFORM.factory(
+        *fi, buttons=[
+            BUTTON("Save Changes", _class="btn btn-primary"),
+            BUTTON("Try Connection", _class="btn btn-info tclient_validate",
+                   _href=URL('default', 'client_settings_validate.load')
+                   )
+            ]
+    )
+    if form.process(hideerror=True).accepted:
+        for a in form.vars:
+            db.global_settings.update_or_insert(db.global_settings.kkey == a, value = form.vars[a], kkey=a)
+
+        settings_.global_settings(refresh=True)
+        session.flash = 'settings updated correctly'
+        redirect(URL(r=request, args=request.args))
+
+    elif form.errors:
+        response.flash = 'errors in form, please check'
+
+    return dict(form=form)
+
+def client_settings_helper():
+    client = request.args(0)
+    if not client or client not in ('transmission', 'deluge', 'utorrent'):
+        return sj.dumps(dict(error="No supported client"))
+
+    mapping = dict(transmission=('admin', 'password', 'http://127.0.0.1:9091/transmission/rpc'),
+                   deluge=('', 'deluge', 'http://127.0.0.1:8112/json'),
+                   utorrent=('admin', 'password', 'http://127.0.0.1:8080/gui/'))
+
+    return sj.dumps(mapping[client])
+
+def client_settings_validate():
+    if request.vars.tclient not in ('transmission', 'deluge', 'utorrent'):
+        return sj.dumps(dict(error="No supported client"))
+    tclient = w2p_tvseries_torrent_client_loader(request.vars.tclient)
+    tclient.username = request.vars.tusername
+    tclient.password = request.vars.tpassword
+    tclient.url = request.vars.turl
+    status = tclient.get_status()
+    if not status:
+        return dict(status='error', message="Connection error, please check it out")
+    else:
+        return dict(status='ok', message='all ok')
