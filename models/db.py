3e1ed9e78cb7a536aae085616d2323bb920b9ab2
niphlod@gmail.com <niphlod@gmail.com>
2013-03-03 20:20:24 +0100

    bumped to version 1.3.9

--- models/db.py
+++ models/db.py
@@ -33,7 +33,7 @@ MIGRATE = True
 LAZY_TABLES = False
 
 response.generic_patterns = ['*'] if request.is_local else []
-response.static_version = '1.3.7'
+response.static_version = '1.3.9'
 
 db = DAL(
     "sqlite://storage.db",
